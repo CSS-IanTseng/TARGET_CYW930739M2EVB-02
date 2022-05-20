98dd8dc5c8ca0f768f32caa61ee80b0a44e63ff4
CSS-IanTseng <Ian.Tseng@infineon.com>
2022-05-20 17:55:18 +0800

    Initialize bsp TARGET_CYW930739M2EVB-02

---
+++ wiced_platform_memory.h
@@ -0,0 +1,32 @@
+/*
+ * $ Copyright 2022-YEAR Cypress Semiconductor $
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief       Allocate memory from the dynamic memory pool.
+ *
+ * @param[in]   size - size of required memory block
+ *
+ * @retval      address of the allocate memory block
+ *
+ */
+void *wiced_platform_memory_allocate(uint32_t size);
+
+/**
+ * \brief       Free a memory block.
+ *
+ * @param[in]   p_addr - start address of the memory block to be freed
+ */
+void wiced_platform_memory_free(void *p_addr);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
---
+++ wiced_platform_os.h
@@ -0,0 +1,23 @@
+/*
+ * $ Copyright 2022-YEAR Cypress Semiconductor $
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief Suspend/Disable the OS thread preemption capability.
+ */
+void wiced_platform_os_preemption_suspend(void);
+
+/**
+ * \brief Resume/Enable the OS thread preemption capability.
+ */
+void wiced_platform_os_preemption_resume(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
